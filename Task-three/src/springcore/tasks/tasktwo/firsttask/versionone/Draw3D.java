b87855e61a666d9f8895b9bcb5349b26dadbc967
RoadmannCoder <mmensheteh@gmail.com>
2025-12-23 04:22:21 +0200

    Added necessary files only for task three

--- Task-three/src/springcore/tasks/tasktwo/firsttask/versionone/Draw3D.java
+++
@@ -1,7 +0,0 @@
-package springcore.tasks.tasktwo.firsttask.versionone;
-
-public class Draw3D {
-    public String draw(Shape shape) {
-        return String.format("Drawing 3D %s", shape.getShapeName());
-    }
-}
--- Task-three/src/springcore/tasks/tasktwo/firsttask/versionone/Shape.java
+++
@@ -1,9 +0,0 @@
-package springcore.tasks.tasktwo.firsttask.versionone;
-
-public interface Shape {
-    void draw();
-    String getShapeName();
-    double getArea(int diemnsion);
-
-
-}
